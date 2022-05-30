4c059ef5df7f4473aadd27311b6da4ba456007e6
Kumaresh <kumareshr38@gmail.com>
2022-05-30 09:43:09 -0700

    third commit

--- hello/src/App.js
+++ hello/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Hi this is kumaresh!
         </p>
         <a
           className="App-link"
